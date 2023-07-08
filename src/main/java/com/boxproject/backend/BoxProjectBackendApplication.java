b752f9dd4a86095ed48107498428b21c93d261d1
Shasi-vns <shashidharshashi52@gmail.com>
2023-07-08 20:40:24 +0530

    uploaded raw fisrt commit

---
+++ src/main/java/com/boxproject/backend/BoxProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boxproject.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxProjectBackendApplication.class, args);
+	}
+
+}
