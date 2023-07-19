ae34fc57f8b83cfc0c47683ae3c4caff68e12cd7
shiva <shivakumarch2000@gmail.com>
2023-07-19 14:04:09 +0530

    dummy commit from shiva

--- src/main/java/com/boxproject/backend/Controller/Controller.java
+++ src/main/java/com/boxproject/backend/Controller/Controller.java
@@ -35,6 +35,4 @@ public class Controller {
         return activityRepo.save(acti);
 
     }
-
-
 }
--- src/main/java/com/boxproject/backend/Entities/Activity.java
+++ src/main/java/com/boxproject/backend/Entities/Activity.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import java.util.Set;
-
+// entity
 @Entity
 public class Activity {
     @Id
