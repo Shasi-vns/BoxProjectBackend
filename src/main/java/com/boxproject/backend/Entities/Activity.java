e729409b94ac440e0a4787b6ad1899ceb031e804
Shasi-vns <shashidharshashi52@gmail.com>
2023-08-17 23:09:59 +0530

    added deleteslot, edit, getActivitiesInParticularCity, api calls and modified Actvity to 1to1 Mapping.

--- src/main/java/com/boxproject/backend/Entities/Activity.java
+++ src/main/java/com/boxproject/backend/Entities/Activity.java
@@ -25,17 +25,10 @@ public class Activity {
     private String activity_owner_name;
     private long activity_ownwer_mobile;
     private String activity_owner_address;
-    @ManyToMany(fetch = FetchType.LAZY,cascade=CascadeType.ALL)
-    @JoinTable(name = "Activity_Slot",
-            joinColumns={
-                    @JoinColumn(name="activity_id_ref",referencedColumnName="activity_id")
-            },
-            inverseJoinColumns={
-                    @JoinColumn(name="slot_id_ref",referencedColumnName="slot_id")
-            }
-    )
-//    @JsonManagedReference
+    @OneToMany(targetEntity = Slots.class,cascade= CascadeType.ALL)
+    @JoinColumn(name="activity_slot",referencedColumnName = "activity_id")
     private Set<Slots> slots;
+
 
     public Integer getActivity_id() {
         return activity_id;
